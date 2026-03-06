3e94671146e80ea492bd2de11b4f90bab4da2710
Ville Seppanen <ville.t.seppanen@iki.fi>
2026-03-07 00:23:28 +0200

    Tweak colors

--- src/render/weapon/render-debug.ts
+++ src/render/weapon/render-debug.ts
@@ -5,8 +5,8 @@ export function renderDebug(parts: LayoutPart[]): string {
     .map(
       (part) => `
         <g class="debug">
-          <rect x="${part.x - part.length / 2}" y="${part.y - part.width / 2}" width="${part.length}" height="${part.width}" fill="none" stroke="rgba(169, 215, 251, 0.45)" stroke-dasharray="6 4" />
-          <circle cx="${part.x}" cy="${part.y}" r="3" fill="rgba(169, 215, 251, 0.8)" />
+          <rect x="${part.x - part.length / 2}" y="${part.y - part.width / 2}" width="${part.length}" height="${part.width}" fill="none" stroke="rgba(247, 225, 31, 0.73)" stroke-dasharray="6 4" />
+          <circle cx="${part.x}" cy="${part.y}" r="4" fill="rgba(239, 148, 11, 0.8)" />
         </g>
       `,
     )
